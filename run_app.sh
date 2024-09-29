ab0ac9c4a9e03df7a6474c145ac9a0096ac8ab32
Aryan8057 <97110288+Aryan8057@users.noreply.github.com>
2024-09-29 15:28:59 +0530

    Update and rename run_app.sh to x.txt

--- run_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set the LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/mount/src/streamlit-ocr-app/tesseract_build/tesseract/leptonica/lib:$LD_LIBRARY_PATH
-
-# Run the Streamlit app
-streamlit run app.py 
